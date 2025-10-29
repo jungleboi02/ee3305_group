5a9f08c7b49de17376d12cff6935392195720d79
jungleboi02 <ethanlock.4d@gmail.com>
2025-10-29 21:34:59 +0800

    Increase max_access_cost parameter value to 500

--- planner.py
+++ planner.py
@@ -37,7 +37,7 @@ class Planner(Node):
         super().__init__(node_name)
 
         # Parameters: Declare
-        self.declare_parameter("max_access_cost", int(140))
+        self.declare_parameter("max_access_cost", int(500))
 
         # Parameters: Get Values
         self.max_access_cost_ = self.get_parameter("max_access_cost").value
